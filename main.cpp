f9b029dfa49a611970c8a9e406c8017347371506
Xabi07 <garveyliam7@gmail.com>
2024-06-15 09:25:31 +0000

    Lab1 code added

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include "mbed.h"
+InterruptIn button(BUTTON1)
+
+void button_interrupt(){
+    printf("button is pressed)";
+}
 
 // main() runs in its own thread in the OS
+
 int main()
 {
     //this is just a comment
     while (true) {
+        button.fall(&button_interrupt();)
 
     }
 }
