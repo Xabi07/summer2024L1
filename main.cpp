575a5e7c1acc8ff8f57c51899fd79fde75d3cd23
Xabi07 <garveyliam7@gmail.com>
2024-06-28 14:02:09 +0000

    fixed typo in comments

--- main.cpp
+++ main.cpp
@@ -3,7 +3,11 @@
 InterruptIn button(BUTTON1);
 volatile int int_trig = 0;
 
-/*This program executes an interrupt, 
+/*This program executes an interrupt, on Button press printf send "Buttonfall" to the screen. Previous versions 
+of the code would not work with printf inside the ISR routine. By adding a volatile variable name to the int_trig variable we 
+are able to execute the function "void button_fall" whenever the BUTTON1 is pressed. When BUTTON1 is pressed int_flag is then set to true or 1
+the code inside the main function executes, before the code prints "buttonfall" the int_trig variable is cleared
+waiting for another interrupt to occur and the programs loops over using the if statement.
 
 */
 void button_fall(){
