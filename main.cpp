962adc2ae97982b07c7e8e1afa431a73197bf4e5
Xabi07 <garveyliam7@gmail.com>
2024-06-28 13:59:38 +0000

    Lab 1 Working Code with Interrupt and If statement

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include "mbed.h"
+#include <cstdio>
 InterruptIn button(BUTTON1);
 volatile int int_trig = 0;
 
+/*This program executes an interrupt, 
+
+*/
 void button_fall(){
-   int trig = 1;
+   int_trig = 1;
 }
 
 // main() runs in its own thread in the OS
@@ -13,7 +17,7 @@ int main()
     //this is just a comment
     button.fall(&button_fall);
     while (true) {
-        if (int_trig ==1){
+        if (int_trig == 1){
             int_trig = 0;
             printf("buttonfall \r\n");
         }
