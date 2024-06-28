6320f33c4c3fce6ea1c728e87677d68bd8b4422e
Xabi07 <garveyliam7@gmail.com>
2024-06-28 14:41:34 +0000

    Added a buttonrise function to detect when the button goes from 0 to 1

--- main.cpp
+++ main.cpp
@@ -14,17 +14,28 @@ void button_fall(){
    int_trig = 1;
 }
 
+void button_rise(){
+    int_trig = 2;
+}
 // main() runs in its own thread in the OS
 
 int main()
 {
     //this is just a comment
     button.fall(&button_fall);
+    button.rise(&button_rise);
     while (true) {
         if (int_trig == 1){
             int_trig = 0;
             printf("buttonfall \r\n");
         }
+        if (int_trig == 2){
+
+            printf("buttonrise \r\n");
+            int_trig = 0;
+
+        }
+        //int_trig = 0;
 
         //button.fall(&button_interrupt);
 
