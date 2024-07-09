a6b04cd818ab14e1165e7e628c6ac5677e2c2bb4
Xabi07 <garveyliam7@gmail.com>
2024-07-09 21:27:55 +0000

    Added comments before uploading lab

--- main.cpp
+++ main.cpp
@@ -10,27 +10,26 @@ the code inside the main function executes, before the code prints "buttonfall" 
 waiting for another interrupt to occur and the programs loops over using the if statement.
 
 */
-void button_fall(){
-   int_trig = 1;
+void button_fall(){     //button_fall function call
+   int_trig = 1;        //sets trig = 1
 }
 
-void button_rise(){
-    int_trig = 2;
+void button_rise(){     //button_rise function call
+    int_trig = 2;       //sets trig = 2
 }
 // main() runs in its own thread in the OS
 
 int main()
 {
     //this is just a comment
-    button.fall(&button_fall);
-    button.rise(&button_rise);
+    button.fall(&button_fall);  //when button is pressed goes from high to low state makes call to the button_fall function
+    button.rise(&button_rise);  //when button is depressed goes from high to low state makes call to the button_rise function
     while (true) {
-        if (int_trig == 1){
+        if (int_trig == 1){     //if trig = 1, set trig = 0 and print buttonfall
             int_trig = 0;
             printf("buttonfall \r\n");
         }
-        if (int_trig == 2){
-
+        if (int_trig == 2){     //if trig = 2, print buttonrise and set trig = 0
             printf("buttonrise \r\n");
             int_trig = 0;
 
